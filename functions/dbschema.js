03c3d6ddec989d8a62f040287a7031c144b2a5f4
Raymond Chan <raymondcja@gmail.com>
2020-01-11 14:37:18 +1000

    added core functionality of signup route

---
+++ functions/dbschema.js
@@ -0,0 +1,8 @@
+let db = {
+    scream: [
+        {
+            userHandle: 'user',
+            body: 'this is the blub body'
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,34 @@
 const functions = require('firebase-functions');
 // our function getBlubs() needs access to the database, using adven sdk
+const app = require('express')();
 const admin = require('firebase-admin'); // to use this admin, we need to initalise our appliation as below
 admin.initializeApp(); // usually for the method we will pass an appliciation into it, but this project already knows that .firebaserc has the project id inside it
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyAWNkmNcLSu_dMgIzFQglD1mW8paoa4uAo",
+  authDomain: "blubberpal-c7253.firebaseapp.com",
+  databaseURL: "https://blubberpal-c7253.firebaseio.com",
+  projectId: "blubberpal-c7253",
+  storageBucket: "blubberpal-c7253.appspot.com",
+  messagingSenderId: "652690348988",
+  appId: "1:652690348988:web:117a3e1bd8e6697434a9b9",
+  measurementId: "G-8P1PNVEE2B"
+};
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hi Pal!");
-});
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
 
-// try to fetch blubs from database to here
-exports.getBlubs = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('blubs').get()
+app.get('/blubs', (req, res) => {
+  admin.firestore().collection('blubs').orderBy('createdAt', 'desc').get()
       .then(data => {
         let blubs = [];
         data.forEach(doc => {
-          blubs.push(doc.data());
+          blubs.push({
+            blubId: doc.id,
+            body: doc.data().body,
+            userHandle: doc.data().userHandle,
+            createdAt: doc.data().createdAt
+          });
         });
         return res.json(blubs);
       })
@@ -27,14 +36,11 @@ exports.getBlubs = functions.https.onRequest((req, res) => {
 });
 
 // now we create another function that creates documents (THIS DOES NOT WORK)
-exports.createBlub = functions.https.onRequest((req, res) => {
-    if(req.method !== 'POST') {
-      return res.status(400).json({ error: 'Method not allowed' });
-    }
+app.post('/blub', (req, res) => {
     const newBlub = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date.toISOString()
     };
 
     admin
@@ -49,3 +55,26 @@ exports.createBlub = functions.https.onRequest((req, res) => {
         console.error(err);
       });
 });
+
+// Signup route
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  // TODO: validate data
+
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res.status(201).json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+});
+
+exports.api = functions.https.onRequest(app);
