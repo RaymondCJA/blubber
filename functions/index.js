91ef0d7c62f6d6a19534e8fb09b82326a178d209
Raymond Chan <raymondcja@gmail.com>
2020-01-20 23:30:03 +1000

    added liking blubs, notifications for likes and comments on blubs

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,8 @@ const app = require("express")();
 
 const FBAuth = require("./util/fbAuth");
 
+const { db } = require("./util/admin");
+
 const {
   getAllBlubs,
   postOneBlub,
@@ -11,7 +13,7 @@ const {
   commentOnBlub,
   likeBlub,
   unlikeBlub,
-  deleteBlub 
+  deleteBlub
 } = require("./handlers/blubs");
 const {
   signup,
@@ -25,9 +27,9 @@ const {
 app.get("/blubs", getAllBlubs);
 app.post("/blub", FBAuth, postOneBlub);
 app.get("/blub/:blubId", getBlub);
-app.delete('/blub/:blubId', FBAuth, deleteBlub);
-app.get('/blub/:blubId/like', FBAuth, likeBlub);
-app.get('/blub/:blubId/unlike', FBAuth, unlikeBlub);
+app.delete("/blub/:blubId", FBAuth, deleteBlub);
+app.get("/blub/:blubId/like", FBAuth, likeBlub);
+app.get("/blub/:blubId/unlike", FBAuth, unlikeBlub);
 app.post("/blub/:blubId/comment", FBAuth, commentOnBlub);
 
 // users routes
@@ -38,3 +40,73 @@ app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region("asia-east2").https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region("asia-east2")
+  .firestore.document("likes/{id}")
+  .onCreate(snapshot => {
+    db.doc(`/blubs/${snapshot.data().blubId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            blubId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+// if someone likes then unlikes your post, the notification would be removed
+exports.deleteNotificationOnUnlike = functions
+  .region("asia-east2")
+  .firestore.document("likes/{id}")
+  .onDelete(snapshot => {
+    db.doc(`notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region("asia-east2")
+  .firestore.document("comments/{id}")
+  .onCreate(snapshot => {
+    db.doc(`/blubs/${snapshot.data().blubId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            blubId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
